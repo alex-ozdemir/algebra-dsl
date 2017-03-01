alex-ozdemir/algebra-dsl

commit 89eb5eace90e6c619e1171839cf04e8ec5517dcb

Fixing division output
Now the whole numerator and whole denominator can be selected.

khwarizmi/src/output.rs
@@ -15,11 +15,11 @@ impl fmt::Display for Math {
 impl fmt::Display for Equation {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "<math xmlns=\"http://www.w3.org/1998/Math/MathML\">")?;
-        write!(f, "<mrow mathTreeNode=\"0\">")?;
+        write!(f, "<{} mathTreeNode=\"0\">", MROW)?;
         fmt_as_math_ml(&self.left, f, "0,0", (&self.left, false, true))?;
         write!(f, "<mo>=</mo>")?;
         fmt_as_math_ml(&self.right, f, "0,1", (&self.right, false, true))?;
-        write!(f, "</mrow>")?;
+        write!(f, "</{}>", MROW)?;
         write!(f, "</math>")
     }
 }
@@ -308,12 +308,35 @@ fn fmt_as_math_ml(expr: &Expression,
             if d.len() == 0 {
                 fmt_prod_as_math_ml(n.as_slice(), f, prev_index, (expr, false, false), 0)?;
             } else {
+
                 write!(f, "<mfrac>")?;
-                write!(f, "<{}>", MROW)?;
-                fmt_prod_as_math_ml(n.as_slice(), f, prev_index, (expr, true, false), 0)?;
+
+                let mut base_string = String::from(prev_index);
+                base_string.push_str(",0");
+
+                write!(f, "<{}{}>", MROW, if n.len() > 1 {
+                    let s = format!(" mathTreeNode=\"{}\"", &base_string);
+                    base_string.push_str(",0");
+                    s
+                } else {
+                    String::new()
+                })?;
+
+                fmt_prod_as_math_ml(n.as_slice(), f, &base_string, (expr, true, false), 0)?;
                 write!(f, "</{}>", MROW)?;
-                write!(f, "<{}>", MROW)?;
-                fmt_prod_as_math_ml(d.as_slice(), f, prev_index, (expr, false, false), n.len())?;
+
+                let mut base_string = String::from(prev_index);
+                base_string.push_str(",1");
+
+                write!(f, "<{}{}>", MROW, if d.len() > 1 {
+                    let s = format!(" mathTreeNode=\"{}\"", &base_string);
+                    base_string.push_str(",0");
+                    s
+                } else {
+                    String::new()
+                })?;
+
+                fmt_prod_as_math_ml(d.as_slice(), f, &base_string, (expr, false, false), n.len())?;
                 write!(f, "</{}>", MROW)?;
                 write!(f, "</mfrac>")?;
             }
@@ -429,7 +452,7 @@ fn fmt_as_math_ml(expr: &Expression,
                prev_precedence.2) {
         write!(f, "<mo form=\"postfix\">)</mo>")?;
     }
-    write!(f, "</mrow>")
+    write!(f, "</{}>", MROW)
 }
 
 fn fmt_prod_as_latex(exprs: &[Expression],

commit 4b66c8df6aae075c7d5c29a547f722bfb8305448

Fixing input to work with Division
89eb5ea broke any selection within a division, this fixes that. Unfortunately,
it's still not possible to select an entire numerator or denominator of a
fraction, because the get and get_mut functions of Indexable for Expresssion
return Expressions, and a numerator is not an Expression. The problem is
contained in clear comments once we figure out what to do.

khwarizmi/src/lib.rs
@@ -707,13 +707,38 @@ impl Indexable for Expression {
                 match self {
                     &Expression::Negation(ref e) if first == 0 => e.get(rest),
                     &Expression::Sum(ref e) if first < e.len() => e[first].get(rest),
-                    &Expression::Division(ref t, ref b) if first < t.len() + b.len() => {
-                        if first < t.len() {
-                            t[first].get(rest)
+                    &Expression::Division(ref t, _) if first == 0 => {
+                        if t.len() == 1 {
+                            t[0].get(rest)
                         } else {
-                            b[first - t.len()].get(rest)
+                            match rest.first() {
+                                Some(snd) if snd < t.len() => {
+                                    let rest = rest.rest();
+                                    t[snd].get(rest)
+                                }
+                                // XXX: user has selected entire top half,
+                                // but can't return that
+                                None => Err(AlgebraDSLError::Unimplemented),
+                                _ => Err(AlgebraDSLError::InvalidIdx),
+                            }
                         }
-                    }
+                    },
+                    &Expression::Division(_, ref b) if first == 1 => {
+                        if b.len() == 1 {
+                            b[0].get(rest)
+                        } else {
+                            match rest.first() {
+                                Some(snd) if snd < b.len() => {
+                                    let rest = rest.rest();
+                                    b[snd].get(rest)
+                                }
+                                // XXX: user has selected entire bottom half,
+                                // but can't return that
+                                None => Err(AlgebraDSLError::Unimplemented),
+                                _ => Err(AlgebraDSLError::InvalidIdx),
+                            }
+                        }
+                    },
                     &Expression::Power(ref base, _) if first == 0 => base.get(rest),
                     &Expression::Power(_, ref power) if first == 1 => power.get(rest),
                     &Expression::Subscript(ref base, _) if first == 0 => base.get(rest),
@@ -744,14 +769,39 @@ impl Indexable for Expression {
                 match self {
                     &mut Expression::Negation(ref mut e) if first == 0 => e.get_mut(rest),
                     &mut Expression::Sum(ref mut e) if first < e.len() => e[first].get_mut(rest),
-                    &mut Expression::Division(ref mut t, ref mut b) if first <
-                                                                       t.len() + b.len() => {
-                        if first < t.len() {
-                            t[first].get_mut(rest)
+                    &mut Expression::Division(ref mut t, _) if first == 0 => {
+                        println!("Getting mut for devision numerator. t: {:?} rest: {:?}", t, rest);
+                        if t.len() == 1 {
+                            t[0].get_mut(rest)
                         } else {
-                            b[first - t.len()].get_mut(rest)
+                            match rest.first() {
+                                Some(snd) if snd < t.len() => {
+                                    let rest = rest.rest();
+                                    t[snd].get_mut(rest)
+                                }
+                                // XXX: user has selected entire top half,
+                                // but can't return that
+                                None => Err(AlgebraDSLError::Unimplemented),
+                                _ => Err(AlgebraDSLError::InvalidIdx),
+                            }
                         }
-                    }
+                    },
+                    &mut Expression::Division(_, ref mut b) if first == 1 => {
+                        if b.len() == 1 {
+                            b[0].get_mut(rest)
+                        } else {
+                            match rest.first() {
+                                Some(snd) if snd < b.len() => {
+                                    let rest = rest.rest();
+                                    b[snd].get_mut(rest)
+                                }
+                                // XXX: user has selected entire bottom half,
+                                // but can't return that
+                                None => Err(AlgebraDSLError::Unimplemented),
+                                _ => Err(AlgebraDSLError::InvalidIdx),
+                            }
+                        }
+                    },
                     &mut Expression::Power(ref mut base, _) if first == 0 => base.get_mut(rest),
                     &mut Expression::Power(_, ref mut power) if first == 1 => power.get_mut(rest),
                     &mut Expression::Subscript(ref mut base, _) if first == 0 => base.get_mut(rest),
@@ -793,6 +843,7 @@ pub enum AlgebraDSLError {
     NeedsExpression,
     UnrecognizedCmd,
     InternalError,
+    Unimplemented,
 }
 
 #[derive(PartialEq, Debug, Clone)]

khwarizmi/src/output.rs
@@ -239,7 +239,9 @@ fn fmt_prod_as_math_ml(exprs: &[Expression],
     let iter = exprs.iter().enumerate();
     for (i, e) in iter {
         let mut base_string = String::from(prev_index);
-        write!(base_string, ",{}", i + start_idx)?;
+        if len > 1 {
+            write!(base_string, ",{}", i + start_idx)?;
+        }
         if i == len - 1 {
             fmt_as_math_ml(e, f, &base_string, prev_precedence)?;
         } else {
@@ -321,9 +323,7 @@ fn fmt_as_math_ml(expr: &Expression,
                 base_string.push_str(",0");
 
                 write!(f, "<{}{}>", MROW, if n.len() > 1 {
-                    let s = format!(" mathTreeNode=\"{}\"", &base_string);
-                    base_string.push_str(",0");
-                    s
+                    format!(" mathTreeNode=\"{}\"", &base_string)
                 } else {
                     String::new()
                 })?;
@@ -335,14 +335,12 @@ fn fmt_as_math_ml(expr: &Expression,
                 base_string.push_str(",1");
 
                 write!(f, "<{}{}>", MROW, if d.len() > 1 {
-                    let s = format!(" mathTreeNode=\"{}\"", &base_string);
-                    base_string.push_str(",0");
-                    s
+                    format!(" mathTreeNode=\"{}\"", &base_string)
                 } else {
                     String::new()
                 })?;
 
-                fmt_prod_as_math_ml(d.as_slice(), f, &base_string, (expr, false, false), n.len())?;
+                fmt_prod_as_math_ml(d.as_slice(), f, &base_string, (expr, false, false), 0)?;
                 write!(f, "</{}>", MROW)?;
                 write!(f, "</mfrac>")?;
             }